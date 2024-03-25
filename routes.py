2bf976366432df795552ad1db3955ee6661e1e24
BreeD2 <dormanb@vcu.edu>
2024-03-25 13:11:46 -0400

    run

--- routes.py
+++ routes.py
@@ -1,11 +1,21 @@
-# reminiscencecollection/models.py
+# reminiscencecollection/reminiscencecollection/routes.py
+from flask import render_template
+from . import app
 
-from . import db
-from flask_login import UserMixin
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-    # Add more fields as needed
+@app.route('/contact/')
+def contact():
+    return render_template('contact/index.html')
+
+@app.route('/signup/')
+def signup():
+    return render_template('signup/index.html')
+
+@app.route('/games/')
+def games():
+    return render_template('games/index.html')
+
+# Define more routes as needed for other pages or game details
