7c3cb601eb8ddc49bd4eb26851e184b3a82b531c
BreeD2 <dormanb@vcu.edu>
2024-03-25 14:21:27 -0400

    change

--- routes.py
+++ routes.py
@@ -2,6 +2,6 @@
 from flask import render_template
 from . import app
 
-@app.route('/signup/')
-def signup():
-    return render_template('signup/index.html')
+@app.route('/')
+def home():
+    return render_template('index.html')
