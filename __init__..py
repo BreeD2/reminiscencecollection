c4627fee6195bb10a152b5a9d915454eb35450dd
BreeD2 <dormanb@vcu.edu>
2024-03-25 13:28:51 -0400

    templates

--- __init__..py
+++ __init__..py
@@ -1,30 +1,9 @@
 # reminiscencecollection/__init__.py
-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import LoginManager
 
 app = Flask(__name__)
-
-# Example configuration
-app.config['SECRET_KEY'] = 'your_secret_key_here'
-app.config['\x16vJ\x8c\x0ecg\xe7$\x0e\x8b\xd6\xab\x9b-['
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# Initialize Flask extensions
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'  # Specify what is the login view
-login_manager.login_message_category = 'info'
 
-from .models import User
-
-@login_manager.user_loader
-def load_user(user_id):
-    """
-    Flask-Login uses this callback to load the current user.
-    """
-    return User.query.get(int(user_id))
-
-# Import routes
-from . import routes
+from reminiscencecollection import models
--- models.py
+++ models.py
@@ -1,11 +1,6 @@
 # reminiscencecollection/models.py
+from reminiscencecollection import db
 
-from . import db
-from flask_login import UserMixin
-
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password_hash = db.Column(db.String(128))
-    # Add more fields as needed
+    # rest of the model
