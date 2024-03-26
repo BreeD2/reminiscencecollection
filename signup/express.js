c79f4d1d2fe427e7de73349b9257db3bccc7d9a3
BreeD2 <dormanb@vcu.edu>
2024-03-26 16:16:00 -0400

    see

--- signup/express.js
+++ signup/express.js
@@ -2,3 +2,9 @@ app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "script-src 'self' https://ajax.googleapis.com https://breed2.github.io; style-src 'self' 'unsafe-inline' https://www.w3schools.com https://cdnjs.cloudflare.com;");
   next();
 });
+
+app.use((req, res, next) => {
+  res.setHeader("Content-Security-Policy", "script-src 'self' https://ajax.googleapis.com https://breed2.github.io; style-src 'self' 'unsafe-inline' https://www.w3schools.com https://cdnjs.cloudflare.com;");
+  // Consider adding CSP reporting for monitoring and debugging: 'report-uri /path-to-report-violations';
+  next();
+});
